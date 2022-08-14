6543986af3408b65b6588809e63d2f1f4b3b3684
Marcelo Magario <marcelomagario@gmail.com>
2022-08-14 21:16:10 +1000

    Create function to convert Celsius to Fahrenreit

--- temperature_converter.py
+++ temperature_converter.py
@@ -1,9 +1,20 @@
 # convert fahrenheit to celsius
 fah = float(input('What is the Fahrenheit temperature you want to convert? '))
 
+
 def transformar_para_celsius():
     celsius = 5 * ((fah - 32)/9)
     print(f'This temperature in Celsius is  {celsius:.0f} C')
 
+
 transformar_para_celsius()
 
+celsius = float(input('What is the Celsius temperature you want to convert? '))
+
+
+def transformar_para_fahrenheit():
+    farenheit = 32 + ((9 * celsius)/5)
+    print(f'This temperature in Fahrenreit is {farenheit:.0f} F')
+
+
+transformar_para_fahrenheit()
