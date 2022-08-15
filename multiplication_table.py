a1867a511a4f73af68ed500a8b64150278dad8e4
Marcelo Magario <marcelomagario@gmail.com>
2022-08-15 18:52:48 +1000

    Create my code of Multiplication Table

--- multiplication_table.py
+++ multiplication_table.py
@@ -1,8 +1,15 @@
-def multiplication_table(n: int) -> str:
-    if 0 < n <= 10:
-        for i in range(1, 11):
+n = int(input('Choose a number between 1 and 10: '))
+if 0 < n <= 10:
+    for i in range(1, 11):
+        print(f'{n} X {i} = {n * i}')
+else:
+    print('Choose number between 1 and 10')
 
-            return f'{n} X {i} = {n * i}'
-    else:
-        return 'Choose number between 1 and 10'
 
+# def multiplication_table(n: int) -> str:
+#     if 0 < n <= 10:
+#         for i in range(1, 11):
+#
+#             return f'{n} X {i} = {n * i}'
+#     else:
+#         return 'Choose number between 1 and 10'
