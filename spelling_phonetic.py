15b2f5536e92a0d6e1c2c3a107a23c8edc9d417b
Marcelo Magario <marcelomagario@gmail.com>
2022-08-30 19:51:58 +1000

    Create my spelling phonetic program

---
+++ spelling_phonetic.py
@@ -0,0 +1,4 @@
+# This is my spelling phonetic program where the user will enter a word that needs to be spelled.
+# The program will show every code word according to NATO Phonetic Alphabetic.
+# Autor: Marcelo Magario
+
