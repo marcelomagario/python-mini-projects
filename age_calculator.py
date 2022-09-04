609f2e2185b8d13cf262e63cdccd7387e69c7514
Marcelo Magario <marcelomagario@gmail.com>
2022-09-04 12:36:40 +1000

    Dividing the program by 2 functions. Verify date and calculate age.

--- age_calculator.py
+++ age_calculator.py
@@ -5,8 +5,30 @@
 
 import datetime
 
-today_is = datetime.datetime.now() # receive today's date
-print(today_is)
-print(f'Today is: {today_is.strftime("%A")} - {today_is.day}/{today_is.month}/{today_is.year}')
-# x = datetime.datetime(2020, 5, 31)
-#dob = date
+input_date = input('Enter your date of birthday (dd/mm/yyyy): ')
+
+
+def check_date(input_date)
+frase = ''
+day, month, year = input_date.split('/')
+valid_date = True
+try:
+    datetime.datetime(int(year), int(month), int(day))
+except ValueError:
+    valid_date = False
+    # if valid_date:
+    #     frase = 'The date is valid.'
+    # else:
+    #     frase = 'Date is invalid, please try again.'
+
+check_date(input_date)
+print(frase)
+
+
+
+
+# today_is = datetime.datetime.now() # receive today's date
+# print(today_is)
+# print(f'Today is: {today_is.strftime("%A")} - {today_is.day}/{today_is.month}/{today_is.year}')
+# # x = datetime.datetime(2020, 5, 31)
+
