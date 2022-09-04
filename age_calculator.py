6bcd2ce4dcde738a2f2c3644c4f98489f1c919b0
Marcelo Magario <marcelomagario@gmail.com>
2022-09-04 12:05:14 +1000

    Updating Age Calculator

--- age_calculator.py
+++ age_calculator.py
@@ -1,6 +1,12 @@
-# Program to calculate the age of the person.
-# Practising using dates.
+# This program will check if the date is valid
+# and it will calculate the age of the person.
+# Practising manipulating dates in Python.
 # Author: Marcelo Magario - 04/09/2022
 
+import datetime
 
-dob = date
+today_is = datetime.datetime.now() # receive today's date
+print(today_is)
+print(f'Today is: {today_is.strftime("%A")} - {today_is.day}/{today_is.month}/{today_is.year}')
+# x = datetime.datetime(2020, 5, 31)
+#dob = date
